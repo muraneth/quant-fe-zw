3e2c9474af7a6179c29886044db1b7d81195dbb3
muraneth <muran.eth@gmail.com>
2024-12-22 12:27:53 +0800

    fix typo

--- src/utils/echarts/y-bar.ts
+++ src/utils/echarts/y-bar.ts
@@ -14,7 +14,7 @@ export function yBarTransform({ indicatorDetailList, priceList, klineType }) {
       },
     },
     xAxis: [
-      getXAxis(klineList),
+      getXAxis(indicatorDetailList),
     ],
 
     yAxis: [],
