1f4c7125ea4d928b5380f0918528ddeb7ccf13a1
muraneth <muran.eth@gmail.com>
2024-12-26 09:51:50 +0800

    x

--- src/pages/explorer/table.tsx
+++ src/pages/explorer/table.tsx
@@ -1,8 +1,17 @@
 import { useMemo } from "react";
 import { Table } from "antd";
 import { useRequest } from "ahooks";
-import { getUserConfig,getTokenSnap,UserConfig,TokenSnapReq } from "@/service/explorer";
-import { TokenDetailInfo, TokenBaseInfo,IndicatorDetailReqDto } from "@/service/charts";
+import {
+  getUserConfig,
+  getTokenSnap,
+  UserConfig,
+  TokenSnapReq,
+} from "@/service/explorer";
+import {
+  TokenDetailInfo,
+  TokenBaseInfo,
+  IndicatorDetailReqDto,
+} from "@/service/charts";
 import { useImmer } from "use-immer";
 import { formatNumber } from "@/utils/common";
 import { useNavigate } from "react-router-dom";
@@ -14,91 +23,105 @@ const TokenTable = () => {
   >([]);
   const [userConfig, setUserConfig] = useImmer<UserConfig>({} as UserConfig);
   const navigate = useNavigate();
-  const [dyColumns , setDyColumns] = useImmer([]);
- useRequest(
-    () =>  getUserConfig(),
-    {
-      onSuccess: (res) => {
-        setUserConfig(res);
-      },
-    }
-  );
+  const [dyColumns, setDyColumns] = useImmer([]);
+  useRequest(() => getUserConfig(), {
+    onSuccess: (res) => {
+      setUserConfig(res);
+    },
+  });
 
-const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+  const sleep = (ms: number) =>
+    new Promise((resolve) => setTimeout(resolve, ms));
 
-useEffect(() => {
-  if (userConfig.indicators && userConfig.tokens) {
-    const indReqTemp = [] as Array<IndicatorDetailReqDto>;
-    for (const ind of userConfig.indicators) {
-      indReqTemp.push({
-        handle_name: ind.handle_name,
-      });
-      setDyColumns((prev) => {
-        return [...prev, {
-          title: ind.name,
-          key: ind.handle_name,
-          render: (_: any, record: TokenDetailInfo) => {
-            const snap = record.indicator_snaps?.find(
-              (snap) => snap.name === ind.name
-            );
-            const baseInfo = record.base_info;
-            return snap ? (
-              <div
-                style={{
-                  
-                  cursor: snap.handle_name ? "pointer" : "default",
-                  color: snap.handle_name ? "gray" : "inherit",
-                }}
-                onClick={() => {
-                  if (snap.handle_name) {
-                    navigate(
-                      `/charts?symbol=${baseInfo.symbol}&handle_name=${snap.handle_name}&type=${'independent_line'}`
-                    );
-                  }
-                }}
-              >
-                <div>{formatNumber(snap.value)}</div>
-                {snap.value_chg !== undefined && snap.value_chg !== 0.0 && (
+  useEffect(() => {
+    if (userConfig.indicators && userConfig.tokens) {
+      const indReqTemp = [] as Array<IndicatorDetailReqDto>;
+      for (const ind of userConfig.indicators) {
+        indReqTemp.push({
+          handle_name: ind.handle_name,
+        });
+        setDyColumns((prev) => {
+          return [
+            ...prev,
+            {
+              title: ind.name,
+              key: ind.handle_name,
+              // sorter: (a, b) => {
+              //   const snapA = a.indicator_snaps?.find((snap) => snap.name === indicator.name);
+              //   const snapB = b.indicator_snaps?.find((snap) => snap.name === indicator.name);
+              //   return (snapA?.value || 0) - (snapB?.value || 0);
+              // },
+              render: (_: any, record: TokenDetailInfo) => {
+                const snap = record.indicator_snaps?.find(
+                  (snap) => snap.name === ind.name
+                );
+                const baseInfo = record.base_info;
+                return snap ? (
                   <div
-                    style={{ color: snap.value_chg > 0 ? "#36F097" : "#EB5757" }}
+                    style={{
+                      cursor: snap.handle_name ? "pointer" : "default",
+                      color: snap.handle_name ? "gray" : "inherit",
+                    }}
+                    onClick={() => {
+                      if (snap.handle_name) {
+                        navigate(
+                          `/charts?symbol=${baseInfo.symbol}&handle_name=${
+                            snap.handle_name
+                          }&type=${"independent_line"}`
+                        );
+                      }
+                    }}
                   >
-                    {snap.value_chg >= 0 ? "+" : ""}
-                    {formatNumber(snap.value_chg * 100)}%
+                    <div>{formatNumber(snap.value)}</div>
+                    {snap.value_chg !== undefined && snap.value_chg !== 0.0 && (
+                      <div
+                        style={{
+                          color: snap.value_chg > 0 ? "#36F097" : "#EB5757",
+                        }}
+                      >
+                        {snap.value_chg >= 0 ? "+" : ""}
+                        {formatNumber(snap.value_chg * 100)}%
+                      </div>
+                    )}
                   </div>
-                )}
-              </div>
-            ) : null;
-          },
-        }]
-      });
-    }
-
-    const processTokens = async () => {
-      for (const token of userConfig.tokens) {
-        try {
-          const result = await runGetTokenSnap({
-            symbol: token,
-            indicators: indReqTemp,
-          } as TokenSnapReq);
-          
-          setTokenDetailList((prev) => {
-            if (!prev.some(item => item.base_info.contract_address === result.base_info.contract_address)) {
-              return [...prev, result];
-            }
-            return prev;
-          });
-
-          await sleep(500);
-          
-        } catch (error) {
-          console.error(`Error processing token ${token}:`, error);
-        }
+                ) : null;
+              },
+            },
+          ];
+        });
       }
-    };
 
-    processTokens();
-  }
-}, [userConfig]);
+      const processTokens = async () => {
+        for (const token of userConfig.tokens) {
+          try {
+            const result = await runGetTokenSnap({
+              symbol: token,
+              indicators: indReqTemp,
+            } as TokenSnapReq);
+
+            setTokenDetailList((prev) => {
+              if (
+                !prev.some(
+                  (item) =>
+                    item.base_info.contract_address ===
+                    result.base_info.contract_address
+                )
+              ) {
+                return [...prev, result];
+              }
+              return prev;
+            });
+
+            await sleep(100);
+          } catch (error) {
+            console.error(`Error processing token ${token}:`, error);
+          }
+        }
+      };
+
+      processTokens();
+    }
+  }, [userConfig]);
 
   const { runAsync: runGetTokenSnap, loading: tokenIndLoading } = useRequest(
     getTokenSnap,
@@ -109,8 +132,8 @@ useEffect(() => {
 
   const columns = useMemo(() => {
     // Base columns for token info
-    console.log("tokenDetailList",tokenDetailList);
-    
+    console.log("tokenDetailList", tokenDetailList);
+
     const baseColumns = [
       {
         title: "Token",
@@ -149,8 +172,49 @@ useEffect(() => {
         render: (createTime: string) => new Date(createTime).toLocaleString(),
       },
     ];
-
-    return [...baseColumns, ...dyColumns];
+    const dynamicColumns =
+      tokenDetailList[0]?.indicator_snaps?.map((indicator, index) => ({
+        title: indicator.name,
+        key: `indicator_${index}`,
+        sorter: (a: TokenDetailInfo, b: TokenDetailInfo) => {
+          const snapA = a.indicator_snaps?.find(
+            (snap) => snap.name === indicator.name
+          );
+          const snapB = b.indicator_snaps?.find(
+            (snap) => snap.name === indicator.name
+          );
+          return (snapA?.value || 0) - (snapB?.value || 0);
+        },
+        render: (_: any, record: TokenDetailInfo) => {
+          const snap = record.indicator_snaps?.find(
+            (snap) => snap.name === indicator.name
+          );
+          const baseInfo = record.base_info;
+          return snap ? (
+            <div
+              style={{
+                cursor: snap.handle_name ? "pointer" : "default",
+                color: snap.handle_name ? "gray" : "inherit",
+              }}
+              onClick={() => {
+                if (snap.handle_name) {
+                  navigate(
+                    `/charts?symbol=${baseInfo.symbol}&handle_name=${snap.handle_name}`
+                  );
+                }
+              }}
+            >
+              <div>{formatNumber(snap.value)}</div>
+              <span
+                style={{ color: snap.value_chg >= 0 ? "#36F097" : "#EB5757" }}
+              >
+                {formatNumber(snap.value_chg * 100)}%
+              </span>
+            </div>
+          ) : null;
+        },
+      })) || [];
+    return [...baseColumns, ...dynamicColumns];
   }, [tokenDetailList]);
 
   return (
@@ -159,9 +223,10 @@ useEffect(() => {
         columns={columns}
         dataSource={tokenDetailList}
         rowKey={(record) => record.base_info.contract_address}
-        loading={tokenIndLoading}
+        // loading={tokenIndLoading}
         bordered
-        scroll={{ x: "max-content" }}
+        scroll={{ x: "max-content", y: 800 }}
+        pagination={false}
       />
     </div>
   );
