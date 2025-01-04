b8712f6b7e8e4738c4e71e5b6e67e0ba8d10d3d6
970027333@qq.com <970027333@qq.com>
2025-01-04 22:38:17 +0800

    wallet超出滚动

--- src/pages/studio/wallet/index.tsx
+++ src/pages/studio/wallet/index.tsx
@@ -1,12 +1,13 @@
 import WalletDetail from "./wallet_detail";
 import WalletTable from "./wallet_table";
+import styles from './index.module.scss';
 
 const Wallets = () => {
   const queryParams = new URLSearchParams(location.search);
   const wallet_address = queryParams.get("wallet_address") as string;
 
   return (
-    <div>
+    <div className={styles.wallet}>
       {wallet_address ? (
         <WalletDetail wallet_address={wallet_address} />
       ) : (
